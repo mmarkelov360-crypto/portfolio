327e655313c29375f70e4f14ea08fef7a02625e1
program <program@sfrudn.local>
2026-03-26 17:12:31 +0300

    Final portfolio build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio',
+  trailingSlash: true,
 };
 
 export default nextConfig;
